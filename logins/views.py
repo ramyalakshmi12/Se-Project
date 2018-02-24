5b51b59254c57e7a3846de0fa71ba3bf23748846
shreyansh <shreyanshrocknroll@gmail.com>
2018-02-24 22:47:27 +0530

    22:40 24-02

--- logins/views.py
+++ logins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 import pyrebase
+from django.contrib import auth
 
 user = {}
 
@@ -14,15 +15,14 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+authe = firebase.auth()
 
 def base(request):
-    keep = auth.get_account_info(user['idToken'])
+    keep = authe.get_account_info(user['idToken'])
 
     '''if not(keep['users'][0]['emailVerified']):
         return 0;'''
     email = keep['users'][0]['email']
-    print(email)
     return render(request, 'base.html')
 
 def signin(request, error = ''):
@@ -36,10 +36,14 @@ def postsignin(request):
     email = request.POST.get('email')
     passw = request.POST.get('password')
     try:
-        user = auth.sign_in_with_email_and_password(email,passw)
+        user = authe.sign_in_with_email_and_password(email,passw)
     except:
         message="invalid info"
         return redirect(reverse(signin))
+    print(user['localId'])
+    session_id = user['localId']
+    request.session['uid'] = str(session_id)
+
     return redirect(reverse(base))
 
 def postsignup(request):
@@ -47,14 +51,15 @@ def postsignup(request):
     email = request.POST.get("email")
     passw = request.POST.get("pass")
     try:
-        user = auth.create_user_with_email_and_password(email,passw)
+        user = authe.create_user_with_email_and_password(email,passw)
     except:
             message="invalid info"
             return render(request,'signup/signup.html',{"messg":message})
+
     return render(request,'base.html',{"e":email})
 
 def logout(request):
-	auth.logout(request)
+	authe.logout(request)
 	return render(request, 'base.html')
 
 def profile(request):
@@ -63,7 +68,7 @@ def profile(request):
 def resetpassword(request):
     email = request.POST.get("email")
     try:
-        user = auth.send_password_reset_email(email)
+        user = authe.send_password_reset_email(email)
     except:
         message="invalid info"
         return render(request, 'base.html', {"messg":messsage})

65cb4bea3ea93ad2462a7d49c1b313339e9e5a4d
ramyalakshmi12 <ramy12797@gmail.com>
2018-03-02 09:59:46 +0530

    10:00 2Mar

--- logins/views.py
+++ logins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 import pyrebase
+from django.contrib import auth as authe
 
 user = {}
 
@@ -17,29 +18,45 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 def base(request):
-    keep = auth.get_account_info(user['idToken'])
+    '''if 'uid' in request.session:
+        keep = auth.get_account_info(request.session['uid'])
+        if keep['users'][0]['emailVerified']:
+            return 0;
+        email = keep['users'][0]['email']'''
+        
+    mesg = ''
+    if 'emailVerificationMesg' in request.session:
+        mesg = request.session.get('emailVerificationMesg')
+        request.session.pop('emailVerificationMesg', None)
+    
+    return render(request, 'base.html', {'mesg': mesg})
 
-    '''if not(keep['users'][0]['emailVerified']):
-        return 0;'''
-    email = keep['users'][0]['email']
-    print(email)
-    return render(request, 'base.html')
-
-def signin(request, error = ''):
-	return render(request,'signin/signin.html', {"error": error})
+def signin(request):
+    if 'mesgcount' in request.session:
+        request.session['mesgcount'] -= 1;
+    else :
+        request.session['mesgcount'] = 0
+    error = ''
+    if request.session.get('mesgcount') > 0:
+        error = request.session.get('mesg')
+    return render(request,'signin/signin.html', {'error': error})
 
 def signup(request):
-	return render(request, 'signup/signup.html')
+    return render(request, 'signup/signup.html')
 
 def postsignin(request):
-    global user
     email = request.POST.get('email')
     passw = request.POST.get('password')
     try:
         user = auth.sign_in_with_email_and_password(email,passw)
     except:
         message="invalid info"
+        request.session['mesg'] = 'Invalid Username or Password'
+        request.session['mesgcount'] = int(2)
         return redirect(reverse(signin))
+    print(user['idToken'])
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
     return redirect(reverse(base))
 
 def postsignup(request):
@@ -48,14 +65,16 @@ def postsignup(request):
     passw = request.POST.get("pass")
     try:
         user = auth.create_user_with_email_and_password(email,passw)
+        auth.send_email_verification(user['idToken'])
+        request.session['emailVerificationMesg'] = 'Verification email has been sent'
     except:
             message="invalid info"
             return render(request,'signup/signup.html',{"messg":message})
-    return render(request,'base.html',{"e":email})
+    return redirect(reverse(base))
 
 def logout(request):
-	auth.logout(request)
-	return render(request, 'base.html')
+    auth.logout(request)
+    return render(request, 'base.html')
 
 def profile(request):
     return render(request, 'profile/profile.html')
